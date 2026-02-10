5c7079569d209e40f383fa6f43cb72a03588328b
Prince <gondaliyaprince668@gmail.com>
2026-02-11 00:06:26 +0530

    Add Microsoft login and password validation

--- login.js
+++ login.js
@@ -1,16 +1,23 @@
-email = document.getElementById("email");
-password = document.getElementById("password");
 const domain = "adaniuni.ac.in";
 
 
 function check() {
-    if (email.value == "" || password.value == "") {
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/;
+
+    if (email == "" || password == "") {
         alert("Email and Password is required");
     }
-    else {
-        alert("Login successful");
-    }
-    if (email.endsWith(domain)) {
+    else if (!email.endsWith(domain)) {
         alert("Email must be adaniuni.ac.in");
     }
+    else if (!passwordRegex.test(password)) {
+        console.log(passwordRegex.test(password));
+        alert("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character");
+    }
+    else {
+        window.location.href = "dashboard.html";
+    }
+
 }
